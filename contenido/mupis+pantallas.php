f372e8dd3344086253de22eaca805ed5a99f0736
administrador User <administrador@ubuntu.ubuntu-domain>
2009-01-20 20:15:57 +0100

    Agregado soporte para edición de pantallas.

--- contenido/mupis+pantallas.php
+++ contenido/mupis+pantallas.php
@@ -5,6 +5,7 @@ function CONTENIDO_pantallas($usuario, $pantalla) {
 	if ( $session->isAdmin() && isset($_POST['registrar_mupi']) ) {
 	//Nos toca registrar un MUPI
 	Pantalla_REGISTRAR();
+	
 	}
 	echo '<hr /><h2>Sus Pantallas '._NOMBRE_.".</h2>";
 	verPantallas($usuario);
@@ -13,8 +14,13 @@ function CONTENIDO_pantallas($usuario, $pantalla) {
 	if ($usuario) {
 		$paraUsuario = " para $usuario";
 	}
-	echo '<hr /><h2>Registrar Pantallas'.$paraUsuario.'</h2>';
-	verPantallasregistro($pantalla);
+	if ($pantalla) {
+		$edicionOregistro = 'Edición de pantalla ' . $pantalla;
+	} else {
+		$edicionOregistro = 'Registrar Pantallas';
+	}
+	echo '<hr /><h2>'.$edicionOregistro.$paraUsuario.'</h2>';
+	verPantallasregistro($usuario, $pantalla);
 	}
 }
 function verPantallas($usuario="", $pantalla=""){
@@ -39,33 +45,55 @@ function verPantallas($usuario="", $pantalla=""){
       return;
    }
 echo '<table>';
-echo "<tr><th>Código Pantalla "._NOMBRE_."</th><th>Código "._NOMBRE_."</th><th>Código propietario</th><th>Alquilado desde'</th><th>Evento</th><th>Foto</th></tr>";
+echo "<tr><th>Código Pantalla "._NOMBRE_."</th><th>Código "._NOMBRE_."</th><th>Código propietario</th><th>Alquilado desde</th><th>Evento</th><th>Foto</th><th>Accion</th></tr>";
    for($i=0; $i<$num_rows; $i++){
-      $codigo_cara_mupi  = CREAR_LINK_GET("gestionar+pantallas&amp;pantalla=".mysql_result($result,$i,"codigo_cara_mupi"),mysql_result($result,$i,"codigo_cara_mupi"), "Editar los datos de esta pantalla");
+      $codigo_cara_mupi  = mysql_result($result,$i,"codigo_cara_mupi");
       $codigo_mupi = mysql_result($result,$i,"codigo_mupi");
       $codigo = mysql_result($result,$i,"codigo");
-      $alquilado_desde  = mysql_result($result,$i,"alquilado_desde");
+      $alquilado_desde  = AnularFechaNula(mysql_result($result,$i,"alquilado_desde"));
       $codigo_evento  = mysql_result($result,$i,"codigo_evento");
       $Foto = mysql_result($result,$i,"Foto");
-      echo "<tr><td>$codigo_cara_mupi</td><td>$codigo_mupi</td><td>$codigo</td><td>$alquilado_desde</td><td>$codigo_evento</td><td>$Foto</td></tr>";
+      $Eliminar = CREAR_LINK_GET("gestionar+pantallas&amp;accion=eliminar&amp;pantalla=".$codigo_cara_mupi,"Eliminar", "Eliminar los datos de esta pantalla");
+      $codigo_cara_mupi  = CREAR_LINK_GET("gestionar+pantallas&amp;pantalla=".$codigo_cara_mupi,$codigo_cara_mupi, "Editar los datos de esta pantalla");
+      echo "<tr><td>$codigo_cara_mupi</td><td>$codigo_mupi</td><td>$codigo</td><td>$alquilado_desde</td><td>$codigo_evento</td><td>$Foto</td><td>$Eliminar</td></tr>";
    }
    echo "</table><br>";
 }
-function verPantallasregistro($pantalla="") {
+function verPantallasregistro($usuario="", $pantalla="") {
 global $form, $database;
+$CampoFechaHora = '';
+$CampoUsuario = '';
+$BotonCancelar = '';
 if ($pantalla) {
 	$q = "SELECT * FROM ".TBL_MUPI_FACES." WHERE codigo_cara_mupi='$pantalla';";
 	$result = $database->query($q);
 	$form->setValue("codigo", mysql_result($result,0,"codigo_cara_mupi"));
 	$form->setValue("foto", mysql_result($result,0,"foto"));
+	$NombreBotonAccion = "Editar";
+	$BotonCancelar = '<input type="button" OnClick="window.location=\'./?'._ACC_.'=gestionar+pantallas\'" value="Cancelar">';
+	$CampoPantalla = '<input type="hidden" name="codigo" value="'.$pantalla.'">';
+	$CampoUsuario = '<tr><td>Cliente:</td><td><input type="text" name="CampoUsuario" style="width: 100%;" maxlength="255" value="' . mysql_result($result,0,"codigo") . '"></td></tr>';
+	$CampoFechaHora = '<tr><td>Fecha de registro:</td><td><input type="text" name="hora" style="width: 100%;" maxlength="255" value="' . AnularFechaNula(mysql_result($result,0,"alquilado_desde")). '"></td></tr>';
+} else {
+	$CampoPantalla = '<tr><td>Código  Pantalla '._NOMBRE_.':</td><td><input type="text" name="codigo" maxlength="100" style="width: 100%;" value="' . $form->value("codigo"). '"></td></tr>';
+	$NombreBotonAccion = "Registrar";
 }
+
+if ($usuario) {
+	$CampoFechaHora = '<tr><td>Fecha de registro:</td><td><input type="text" name="hora" style="width: 100%;" maxlength="255" value="' . date("d-m-Y", time()) . '"></td></tr>';
+	$CampoUsuario  = '<input type="hidden" name="CampoUsuario" value="'.$usuario.'">';
+}
+
 echo '
 <form action="./?'._ACC_.'=gestionar+pantallas" method="POST">
 <table>
-<tr><td>Código  Pantalla '._NOMBRE_.':</td><td><input type="text" name="codigo" maxlength="100" style="width: 100%;" value="' . $form->value("codigo"). '"></td></tr>
+'.$CampoPantalla.'
+'.$CampoUsuario.'
 <tr><td>Foto:</td><td><input type="text" name="foto" style="width: 100%;" maxlength="255" value="' . $form->value("foto"). '"></td></tr>
+'.$CampoFechaHora.'
 </table>
-<input type="submit" value="Registrar">
+<input type="submit" value="'.$NombreBotonAccion.'">
+'.$BotonCancelar.'
 <input type="hidden" name="registrar_mupi" value="1">
 </form>';
 }
@@ -74,8 +102,12 @@ function Pantalla_REGISTRAR() {
 global $database,$form;
 $form->setValue("codigo", $_POST['codigo']);
 $form->setValue("foto", $_POST['foto']);
+if ( isset($_POST['CampoUsuario'] ) ) {
+	$q = "INSERT INTO ".TBL_MUPI_FACES." (codigo_cara_mupi, codigo, foto, alquilado_desde) VALUES ('".$_POST['codigo'] . "', '".$_POST['CampoUsuario'] . "', '" . $_POST['foto'] ."', '" . strtotime($_POST['hora']) ."')  ON DUPLICATE KEY UPDATE codigo_cara_mupi=VALUES(codigo_cara_mupi), codigo=VALUES(codigo), foto=VALUES(foto), alquilado_desde=VALUES(alquilado_desde);";
+} else {
+	$q = "INSERT INTO ".TBL_MUPI_FACES." (codigo_cara_mupi, foto) VALUES ('".$_POST['codigo'] . "', '" . $_POST['foto'] ."')  ON DUPLICATE KEY UPDATE codigo_cara_mupi=VALUES(codigo_cara_mupi), foto=VALUES(foto);";
+}
 echo "Registrado ".  $_POST['codigo'];
-$q = "INSERT INTO ".TBL_MUPI_FACES." (codigo_cara_mupi, foto) VALUES ('".$_POST['codigo'] . "', '" . $_POST['foto'] ."')  ON DUPLICATE KEY UPDATE codigo_cara_mupi=VALUES(codigo_cara_mupi), foto=VALUES(foto);";
 DEPURAR ($q);
 $result = $database->query($q);
 }
