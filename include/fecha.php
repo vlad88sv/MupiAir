0ae34eaf2424fa75935da1ed0341ad1e9c4b490e
administrador User <administrador@ubuntu.ubuntu-domain>
2009-01-26 09:08:21 -0600

    Agregada funcion Combobox_catorcenas($referencia=time()) Obtiene la catorcena PASADA mas cercana a la fecha de referencia.

--- include/fecha.php
+++ include/fecha.php
@@ -1,10 +1,16 @@
 <?php
+/* Configuración */
+$MesInicio = 1;
+$DiaInicio = 13;
+
+/*****************************/
 function Combobox_catorcenas($nombre="catorcena", $default=NULL, $cuantas = NULL) {
+global $MesInicio, $DiaInicio;
 if ( !$default ) { $default = time(); }
 if ( $cuantas ) {
 	$inicio=mktime(0,0,0,date('n',$default),1,date('Y', $default));
 } else {
-	$inicio=mktime(0,0,0,1,1,date('Y', $default));
+	$inicio=mktime(0,0,0,$MesInicio,$DiaInicio,date('Y', $default));
 	$cuantas = 26;
 }
 $s='<select name="'.$nombre.'">';
@@ -17,4 +23,17 @@ for ($i=0; $i<$cuantas; $i++){
 $s.= '</select>';
 return $s;
 }
+
+function Obtener_catorcena_cercana ($referencia = NULL ) {
+global $MesInicio, $DiaInicio;
+if ( !$referencia ) { $referencia = time(); }
+$inicio=mktime(0,0,0,$MesInicio,$DiaInicio,date('Y', $referencia));
+for ($i=0; $i<27; $i++){
+  $catorcena = strtotime("+13 day",$inicio);
+  if ( ($referencia >= $inicio) && ($referencia <= $catorcena) ){ return $inicio; }
+  $inicio =  strtotime("+1 day",$catorcena);
+}
+return NULL;
+}
+
 ?>
