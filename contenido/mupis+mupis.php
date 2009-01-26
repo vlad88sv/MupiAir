154b04acd4ff9462ea5b5fcc02c8859205ac040d
administrador User <administrador@ubuntu.ubuntu-domain>
2009-01-26 09:20:17 -0600

    Especificaciones de los campos y estilo.

--- contenido/mupis+mupis.php
+++ contenido/mupis+mupis.php
@@ -84,10 +84,10 @@ echo '
 <form action="./?'._ACC_.'=gestionar+mupis" method="POST">
 <table>
 '.$CampoCodigoMupi.'
-<tr><td>Dirección:</td><td><input type="text" name="direccion" style="width: 100%;" maxlength="255" value="' . $form->value("direccion"). '"></tr>
+<tr><td width="20%">Dirección específica:</td><td><input type="text" name="direccion" style="width: 100%;" maxlength="255" value="' . $form->value("direccion"). '"></tr>
 <tr><td>Foto genérica:</td><td><input type="text" name="foto" style="width: 100%;" maxlength="255" value="' . $form->value("foto"). '"></td></tr>
-<tr><td>Longitud:</td><td><input type="text" name="lon" style="width: 100%;" maxlength="50" value="' . $form->value("lon"). '"></td></tr>
-<tr><td>Latitud:</td><td><input type="text" name="lat" style="width: 100%;" maxlength="50" value="' . $form->value("lat"). '"></td></tr>
+<tr><td>Longitud Decimal:</td><td><input type="text" name="lon" style="width: 100%;" maxlength="50" value="' . $form->value("lon"). '"></td></tr>
+<tr><td>Latitud Decimal:</td><td><input type="text" name="lat" style="width: 100%;" maxlength="50" value="' . $form->value("lat"). '"></td></tr>
 <tr><td>Código calle:</td><td>'. $database->Combobox_calle("codigo_calle", $form->value("codigo_calle")). '</td></tr>
 </table>
 <input type="submit" value="'.$NombreBotonAccion.'">
