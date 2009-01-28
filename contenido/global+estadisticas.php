ed93698b4452e57c97be71845b40a27a1e108638
administrador User <administrador@ubuntu.ubuntu-domain>
2009-01-28 21:41:08 +0100

    Estilo en las estadísticas.

--- contenido/global+estadisticas.php
+++ contenido/global+estadisticas.php
@@ -6,19 +6,19 @@ if ( $session->isAdmin() ) {
 }
 echo "<h1>Estadísticas</h1>";
 
-echo "Catorcena actual: " . date("d/m/Y", Obtener_catorcena_cercana()) . ' a ' . date("d/m/Y", Fin_de_catorcena(Obtener_catorcena_cercana())) . "<br />";
+echo "Catorcena actual: <b>" . date("d/m/Y", Obtener_catorcena_cercana()) . ' a ' . date("d/m/Y", Fin_de_catorcena(Obtener_catorcena_cercana())) . "</b><br />";
 
 $q = "SELECT COUNT(*) as cuenta FROM ". TBL_MUPI_FACES ." WHERE catorcena=".Obtener_catorcena_cercana()." AND codigo_pedido IN (SELECT codigo_pedido from ".TBL_MUPI_ORDERS." WHERE codigo = '".$session->codigo."');";
 $result = $database->query($q);
-echo "Número de caras publicitarias contratadas en catorcena actual: " . mysql_result($result,0,"cuenta")."<br />";
+echo "Número de caras publicitarias contratadas en catorcena actual: <b>" . mysql_result($result,0,"cuenta")."</b><br />";
 
 $q = "SELECT SUM(catorcena_fin - catorcena_inicio) as cuenta FROM emupi_mupis_pedidos WHERE codigo='".$session->codigo."';";
 $result = $database->query($q);
-echo "Número de catorcenas contratadas: " . Contar_catorcenas(mysql_result($result,0,"cuenta"))."<br />";
+echo "Número de catorcenas contratadas: <b>" . Contar_catorcenas(mysql_result($result,0,"cuenta"))."</b><br />";
 
 $q = "SELECT SUM((SELECT impactos FROM " . TBL_STREETS . " WHERE codigo_calle = (SELECT codigo_calle FROM ".TBL_MUPI." AS c WHERE c.codigo_mupi=a.codigo_mupi))) AS 'Impactos' FROM ". TBL_MUPI_FACES ." AS a WHERE catorcena=".Obtener_catorcena_cercana()." AND codigo_pedido IN (SELECT codigo_pedido FROM ".TBL_MUPI_ORDERS." WHERE codigo='".$session->codigo."')".";";
 $result = $database->query($q);
-echo "Número de impactos publicitarios diarios: " . (int) (mysql_result($result,0,"Impactos"))."<br />";
+echo "Número de impactos publicitarios diarios: <b>" . (int) (mysql_result($result,0,"Impactos"))."</b><br />";
 
 /*********************************************************************************************/
 // Inicio de parte dinámica.
