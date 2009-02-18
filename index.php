dffc444f86d2e10aef323bb27ef5ae99164f8e1b
administrador User <administrador@ubuntu.ubuntu-domain>
2009-02-18 18:54:37 +0100

    Mejoras visuales y espere por favor...

--- index.php
+++ index.php
@@ -66,6 +66,63 @@ require_once("contenido/mapa+referencias.php");
 	* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
 	***********************************************/
 	</script>
+	<script>
+	function grayOut(vis, options) {
+  // Pass true to gray out screen, false to ungray
+  // options are optional.  This is a JSON object with the following (optional) properties
+  // opacity:0-100         // Lower number = less grayout higher = more of a blackout 
+  // zindex: #             // HTML elements with a higher zindex appear on top of the gray out
+  // bgcolor: (#xxxxxx)    // Standard RGB Hex color code
+  // grayOut(true, {'zindex':'50', 'bgcolor':'#0000FF', 'opacity':'70'});
+  // Because options is JSON opacity/zindex/bgcolor are all optional and can appear
+  // in any order.  Pass only the properties you need to set.
+  var options = options || {}; 
+  var zindex = options.zindex || 50;
+  var opacity = options.opacity || 70;
+  var opaque = (opacity / 100);
+  var bgcolor = options.bgcolor || '#000000';
+  var dark=document.getElementById('darkenScreenObject');
+  if (!dark) {
+    // The dark layer doesn't exist, it's never been created.  So we'll
+    // create it here and apply some basic styles.
+    // If you are getting errors in IE see: http://support.microsoft.com/default.aspx/kb/927917
+    var tbody = document.getElementsByTagName("body")[0];
+    var tnode = document.createElement('div');           // Create the layer.
+        tnode.style.position='absolute';                 // Position absolutely
+        tnode.style.top='0px';                           // In the top
+        tnode.style.left='0px';                          // Left corner of the page
+        tnode.style.overflow='hidden';                   // Try to avoid making scroll bars            
+        tnode.style.display='none';                      // Start out Hidden
+        tnode.id='darkenScreenObject';                   // Name it so we can find it later
+    tbody.appendChild(tnode);                            // Add it to the web page
+    dark=document.getElementById('darkenScreenObject');  // Get the object.
+  }
+  if (vis) {
+    // Calculate the page width and height 
+    if( document.body && ( document.body.scrollWidth || document.body.scrollHeight ) ) {
+        var pageWidth = document.body.scrollWidth+'px';
+        var pageHeight = document.body.scrollHeight+'px';
+    } else if( document.body.offsetWidth ) {
+      var pageWidth = document.body.offsetWidth+'px';
+      var pageHeight = document.body.offsetHeight+'px';
+    } else {
+       var pageWidth='100%';
+       var pageHeight='100%';
+    }   
+    //set the shader to cover the entire page and make it visible.
+    dark.style.opacity=opaque;                      
+    dark.style.MozOpacity=opaque;                   
+    dark.style.filter='alpha(opacity='+opacity+')'; 
+    dark.style.zIndex=zindex;        
+    dark.style.backgroundColor=bgcolor;  
+    dark.style.width= pageWidth;
+    dark.style.height= pageHeight;
+    dark.style.display='block';                          
+  } else {
+     dark.style.display='none';
+  }
+}
+	</script>
 	<!--[if IE 6]>
 	<style type="text/css">
 	/* some css fixes for IE browsers */
@@ -77,7 +134,10 @@ require_once("contenido/mapa+referencias.php");
 	<![endif]-->
 </head>
 <body>
-<div id="bg"><img src="fondo.jpg" width="100%" height="100%" alt=""></div>
+<div id="cargando"><img src="loader.gif"> Por favor espere...<hr /></div>
+<script>grayOut(true)</script>
+<?php flush(); ?>
+<?php ob_start(); ?>
 <div id="container">
 	<table style="border:0">
 		 <tr>
@@ -115,3 +175,10 @@ require_once("contenido/mapa+referencias.php");
 	</div>
 	</body>
 </html>
+<?php ob_flush(); flush(); ?>
+<script>
+window.setTimeout(function() {
+ $("#cargando").html("")
+ grayOut(false)
+}, 1000);
+</script>
