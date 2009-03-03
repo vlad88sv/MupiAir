2f18ff4f75fe927645139207b8ea9ae1888090cf
administrador User <administrador@ubuntu.ubuntu-domain>
2009-03-03 21:45:49 +0100

    removido codigo antiguo.

--- contenido/usuario.php
+++ contenido/usuario.php
@@ -24,7 +24,6 @@ function displayUsers(){
       $time   = date("d-m-y\nh:ia", mysql_result($result,$i,"timestamp"));
       $verPedidos = CREAR_LINK_GET("gestionar+pedidos:$uname", "Pedidos", "Le mostrara los pedidos realizados por este cliente y le dará la opción de agregar más.");
       $verPantallas = CREAR_LINK_GET("gestionar+pantallas:$uname", "Pantallas", "Le mostrara las pantallas en las cuales se encuentran colocados los pedidos.");
-      //$verMUPIS = CREAR_LINK_GET("gestionar+mupis:$uname", "MUPIS", "Le mostrara los MUPIS donde estan instaladas las pantallas de este cliente.");
       $verUbicaciones = CREAR_LINK_GET("ver+ubicaciones:$uname", "Ubicaciones", "Le mostrara las ubicaciones de los MUPIS de este cliente.");
       $reporte = CREAR_LINK_GET("ver+reportes:$uname", "Reporte", "Le generará un reporte sobre este cliente.");
       $uname = CREAR_LINK_GET("ver+cliente:".$uname, $uname, "Ver datos de este cliente");
