f5a25f866e01fc7be29e6e420ef6fcc72f771232
administrador User <administrador@ubuntu.ubuntu-domain>
2009-03-13 09:54:26 -0600

    Reparado reporte de error personalizado 404

--- contenido/global+404.php
+++ contenido/global+404.php
@@ -3,7 +3,7 @@ function CONTENIDO_global_404() {
 echo
 '<h1>'._NOMBRE_.' - ¡Error!</h1><br />
 ¡Lo sentimos pero Ud. ha intentado ingresar a un área de '._NOMBRE_.' que no existe!.<br />
-Ud. intentó ingresar a <b>"'.$_SERVER['REQUEST_URI'] .'"</b>.<br />
+Ud. intentó ingresar a <b>"'.htmlspecialchars(urldecode($_SERVER['REQUEST_URI'])) .'"</b>.<br />
 <br />Sin embargo un reporte de error fue generado y enviado automáticamente a los administradores de '
 ._NOMBRE_. ' para corroborar el problema.
 ';
